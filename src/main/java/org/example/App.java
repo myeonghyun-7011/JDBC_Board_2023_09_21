6d55126824c5dedace042f28d5aa00d83e08d0fb
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-09-21 17:17:39 +0900

    게시물 작성시 DB에 저장

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -25,10 +29,62 @@ public class App {
         /*int id = articleLastId + 1 ;
         articleLastId++;*/
 
+//-------------------연결 시작문----------------------------------------------------------------------------------------------
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try{
+          Class.forName("com.mysql.jdbc.Driver");
+
+          String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+          conn = DriverManager.getConnection(url, "sbsst", "sbs123414");
+
+          String sql = "INSERT INTO article";
+          sql += " SET regDate = NOW()";
+          sql += ", updateDate = NOW()";
+          sql += " , title = \"" + title + "\" ";
+          sql += " , `body` = \"" + body + "\" ";
+
+          System.out.println("sql : " + sql);
+
+          pstmt = conn.prepareStatement(sql);
+          int affectedRows = pstmt.executeUpdate();
+
+
+          System.out.println("affectedRows : " + affectedRows);
+
+        }
+        catch(ClassNotFoundException e){
+          System.out.println("드라이버 로딩 실패");
+        }
+        catch(SQLException e){
+          System.out.println("에러: " + e);
+        }
+        finally{
+          try{
+            if( conn != null && !conn.isClosed()){
+              conn.close();
+            }
+          }
+          catch( SQLException e){
+            e.printStackTrace();
+          }
+          try{
+            if( pstmt != null && !pstmt.isClosed()){
+              pstmt.close();
+            }
+          }
+          catch( SQLException e){
+            e.printStackTrace();
+          }
+        }
+//-----------------연결끝문-------------------------------------------------------------------------------------------------
+
+
         Article article = new Article(id, title, body);
         articles.add(article);
         // 바깥쪽에 List<Article> articles = new ArrayList<>(); 생성해서 하나씩정장
-
 
         System.out.println("생성된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);

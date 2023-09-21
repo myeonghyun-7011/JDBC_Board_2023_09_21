af5e96c76df08d49d49695ba92fa71e82128039c
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-09-21 15:25:12 +0900

    기본적인 게시물 리스팅,추가기능구현

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,25 +1,77 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
   public void run() {
     Scanner sc = Container.scanner;
 
-    while (true){
+    List<Article> articles = new ArrayList<>();
+    int articleLastId = 0;
+
+    while (true) {
       System.out.printf("명령어) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("article list")) {
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println((" == 게시물 등록  ==="));
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+        int id = ++articleLastId;//같은뜻
+        /*int id = articleLastId + 1 ;
+        articleLastId++;*/
+
+        Article article = new Article(id, title, body);
+        articles.add(article);
+        // 바깥쪽에 List<Article> articles = new ArrayList<>(); 생성해서 하나씩정장
+
+
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+      } else if (cmd.equals("/usr/article/list")) {
         System.out.println((" == 게시물 리스트  ==="));
-      }
-      else if(cmd.equals("system exit")){
+
+        if (articles.isEmpty()) {
+          System.out.println("등록된 게시물이 없습니다.");
+          continue;
+        }
+        System.out.println("번호 / 제목");
+        for (Article article : articles) {
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+
+      } else if (cmd.equals("system exit")) {
         System.out.println("시스템 종료");
-        break;}
-      else {
+        break;
+      } else {
         System.out.println("명령어를 확인해주세요.");
       }
     }
     sc.close();
+  }
+}
+
+class Article {
+  public int id;
+  public String title;
+  public String body;
+
+  public Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+
+  @Override //  개발자 확인겸 디버깅활동에 좋은거
+  public String toString() {
+    return "Article{" +
+        "id=" + id +
+        ", title='" + title + '\'' +
+        ", body='" + body + '\'' +
+        '}';
   }
 }

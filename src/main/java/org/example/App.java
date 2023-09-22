b765947a54c6d1a6b500273296b6657824767fb2
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-09-22 10:09:38 +0900

    게시물 리스트 데이터를 DB에서 가져오기

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,8 +8,6 @@ import java.util.Scanner;
 public class App {
   public void run() {
     Scanner sc = Container.scanner;
-
-    List<Article> articles = new ArrayList<>();
     int articleLastId = 0;
 
     while (true) {
@@ -29,11 +24,11 @@ public class App {
         /*int id = articleLastId + 1 ;
         articleLastId++;*/
 
-//-------------------연결 시작문----------------------------------------------------------------------------------------------
+//-------------------JDBCincline 연결 시작문----------------------------------------------------------------------------------------------
         Connection conn = null;
         PreparedStatement pstmt = null;
 
-        try{
+        try {
           Class.forName("com.mysql.jdbc.Driver");
 
           String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
@@ -51,44 +46,104 @@ public class App {
           pstmt = conn.prepareStatement(sql);
           int affectedRows = pstmt.executeUpdate();
 
+          // System.out.println("affectedRows : " + affectedRows);
 
-          System.out.println("affectedRows : " + affectedRows);
-
-        }
-        catch(ClassNotFoundException e){
+        } catch (ClassNotFoundException e) {
           System.out.println("드라이버 로딩 실패");
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
           System.out.println("에러: " + e);
-        }
-        finally{
-          try{
-            if( conn != null && !conn.isClosed()){
+        } finally {
+          try {
+            if (conn != null && !conn.isClosed()) {
               conn.close();
             }
-          }
-          catch( SQLException e){
+          } catch (SQLException e) {
             e.printStackTrace();
           }
-          try{
-            if( pstmt != null && !pstmt.isClosed()){
+          try {
+            if (pstmt != null && !pstmt.isClosed()) {
               pstmt.close();
             }
-          }
-          catch( SQLException e){
+          } catch (SQLException e) {
             e.printStackTrace();
           }
         }
-//-----------------연결끝문-------------------------------------------------------------------------------------------------
+//-----------------JDBCincline끝문-------------------------------------------------------------------------------------------------
 
-
-        Article article = new Article(id, title, body);
+        /*Article article = new Article(id, title, body);
         articles.add(article);
-        // 바깥쪽에 List<Article> articles = new ArrayList<>(); 생성해서 하나씩정장
+       // 바깥쪽에 List<Article> articles = new ArrayList<>(); 생성해서 하나씩저장
+        System.out.println("생성된 게시물 객체 : " + article);*/
 
-        System.out.println("생성된 게시물 객체 : " + article);
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
       } else if (cmd.equals("/usr/article/list")) {
+
+//-------------------JDBCSELECTTEST 시작문----------------------------------------------------------------------------------------------        Connection conn = null;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        List<Article> articles = new ArrayList<>();
+
+        try {
+          Class.forName("com.mysql.jdbc.Driver");
+
+          String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+          conn = DriverManager.getConnection(url, "sbsst", "sbs123414");
+
+          String sql = " SELECT *";
+          sql += " FROM article";
+          sql += " ORDER BY id DESC";
+
+          System.out.println("sql : " + sql);
+
+
+          pstmt = conn.prepareStatement(sql);
+          rs = pstmt.executeQuery(sql);
+
+          while (rs.next()) {  // 다음장으로 넘김.
+            int id = rs.getInt("id");
+            String regDate = rs.getString("regDate");
+            String updateDate = rs.getString("updateDate");
+            String title = rs.getString("title");
+            String body = rs.getString("body");
+
+            Article article = new Article(id, regDate, updateDate, title, body);
+            articles.add(article);
+
+          }
+          System.out.println("결과 : " + articles);
+
+        } catch (ClassNotFoundException e) {
+          System.out.println("드라이버 로딩 실패");
+        } catch (SQLException e) {
+          System.out.println("에러: " + e);
+        } finally {
+          try {
+            if (rs != null && !rs.isClosed()) {
+              rs.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+          try {
+            if (pstmt != null && !pstmt.isClosed()) {
+              pstmt.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+          try {
+            if (conn != null && !conn.isClosed()) {
+              conn.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+        }
+//-----------------JDBCSELECTTEST 끝문-------------------------------------------------------------------------------------------------
+
         System.out.println((" == 게시물 리스트  ==="));
 
         if (articles.isEmpty()) {
@@ -115,9 +170,19 @@ class Article {
   public int id;
   public String title;
   public String body;
+  public String regDate;
+  public String updateDate;
 
   public Article(int id, String title, String body) {
     this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+
+  public Article(int id, String regDate, String updateDate, String title, String body) {
+    this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
     this.title = title;
     this.body = body;
   }
